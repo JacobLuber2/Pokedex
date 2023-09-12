7c2a191a649987d75f5d8b8a6b1d9acce2f2c112
JacobLuber2 <jaluber10@gmail.com>
2023-09-12 09:08:39 -0600

    finished

--- src/detailspage.jsx
+++ src/detailspage.jsx
@@ -1,8 +1,39 @@
-function detailsPage(pokemon, props) {
-return(
-<div>
-<div>{pokemon.img}</div>
-<button>Back</button>
-</div>
-)
+import React, { useEffect, useState } from "react";
+import { useParams, useNavigate } from "react-router-dom";
+
+export default function DetailsPage() {
+    const [activePokemon, setActivePokemon] = useState(null);
+    const { name } = useParams();
+    const navigate = useNavigate();
+
+    useEffect(() => {
+        function fetchPokemon() {
+            fetch(
+              "https://raw.githubusercontent.com/Biuni/PokemonGO-Pokedex/master/pokedex.json"
+            )
+              .then((res) => res.json())
+              .then((pokemon) => {
+                const active = pokemon.pokemon.find(pokemon => pokemon.name === name);
+                setActivePokemon(active);
+              })
+        
+              .catch((err) => console.error(err));
+          }
+
+        fetchPokemon();
+    }, []);
+
+    function handleBack () {
+        navigate(-1);
+    }
+    
+  return (
+    <div>
+        <p>
+            {activePokemon?.name}
+        </p>
+      <img src={activePokemon?.img} />
+      <button onClick={handleBack}>Back</button>
+    </div>
+  );
 }
